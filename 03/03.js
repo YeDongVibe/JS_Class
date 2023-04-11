7aef8d28b11cbfb5331381509a6744e830f372d2
YeDongVibe <wkwhstladmss@naver.com>
2023-04-11 12:20:22 +0900

    수정

--- 03/03.js
+++ 03/03.js
@@ -16,20 +16,60 @@
 const show = () => { //show함수에 값 대입 // 화살표 함수로 지정(요즘 많이 사용함)
     event.preventDefault();
     // document.getElementById("h2Id").innerHTML = "안녕하세요." ;
-    document.getElementById("h2Id").innerHTML = "<img src = '../img/1.png'></img> "; //이미지 삽입
-}
+    // document.getElementById("h2Id").innerHTML = "<img src = '../img/1.png'></img> "; //이미지 삽입
+
+    //랜덤수 생성 (1~6)
+    let n = Math.floor(Math.random() * 6) + 1 ;
+    console.log(n); //사용자 눈에는 안보이는 프린트아웃.
+
+    //해당하는 파일을 각 수에 맞게 출력.
+    // document.getElementById("h2Id").innerHTML = `<img src = "../img/${n}.png">`;
+
+    //숫자에 해당되는 이미지 출력
+    // document.getElementById("h2Id").innerHTML = "<img src = '../img/' + n +'.png'></img> ";
+
+    //if구문 사용
+    // let imgTag;
+    // if (n ==1)  imgTag = "<img src = '../img/1.png'></img> ";
+    // else if (n ==2)  imgTag = "<img src = '../img/2.png'></img> ";
+    // else if (n ==3)  imgTag = "<img src = '../img/3.png'></img> ";
+    // else if (n ==4)  imgTag = "<img src = '../img/4.png'></img> ";
+    // else if (n ==5)  imgTag = "<img src = '../img/5.png'></img> ";
+    // else if (n ==6)  imgTag = "<img src = '../img/6.png'></img> ";
+
+    //switch 구문 사용
+    // switch (n){
+    //     case 1 : imgTag = "<img src = '../img/1.png'></img> "; break;
+    //     case 2 : imgTag = "<img src = '../img/2.png'></img> "; break;
+    //     case 3 : imgTag = "<img src = '../img/3.png'></img> "; break;
+    //     case 4 : imgTag = "<img src = '../img/4.png'></img> "; break;
+    //     case 5 : imgTag = "<img src = '../img/5.png'></img> "; break;
+    //     case 6 : imgTag = "<img src = '../img/6.png'></img> "; break;
+    // }
+
+
+    //배열 사용
+    // let imgArr =["<img src = '../img/1.png'></img> ", 
+    // "<img src = '../img/2.png'></img> ", 
+    // "<img src = '../img/2.png'></img> ", 
+    // "<img src = '../img/4.png'></img> ", 
+    // "<img src = '../img/5.png'></img> ", 
+    // "<img src = '../img/6.png'></img> "]
+    // document.getElementById("h2Id").innerHTML = imgArr[n-1];
+
+    // document.getElementById("h2Id").innerHTML = '<img.src = "../img/1.png">';
 
 // document.addEventListener("DOMContentLoaded", function(){
 //     document.getElementById("h2Id").innerHTML = "시작입니다." ;
 // }); //돔컨텐트가 로드되면 (돔트리가 전체 구성 후) 어떤 이벤트가 발생하는건가?라고 지정해줌.
 
-document.addEventListener("DOMContentLoaded", () => { //위의 코드를 화살표 함수로 지정하여 표현한 방식
+// document.addEventListener("DOMContentLoaded", () => { //위의 코드를 화살표 함수로 지정하여 표현한 방식
     // document.getElementById("h2Id").innerHTML = "시작입니다." ;
+// });
 
     // document.querySelector('#h2Id').innerHTML = <"아이디 선택자 : 시작입니다."> ; // = document.getElementById("h2Id").innerHTML
     // document.querySelector('.h2class').innerHTML = "클래스 선택자 : 시작입니다.";
-    // const bt = document.createElement('button'); //a라는 버튼을 생성.
+    // const bt = document.createElement('button'); //a라는 버튼을 생성. //cons는 상수라 바뀌면 안된다. 즉 변화없음.
     // bt.textContent = "확인" ; //버튼의 이름 지정.
     // document.getElementById('hdiv').append(bt); //hdiv위치에 버튼을 추가(append)하는것.
-
-});
+}
