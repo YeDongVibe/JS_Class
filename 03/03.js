10eb39ee31436235258670841427d0a30c4a4856
YeDongVibe <wkwhstladmss@naver.com>
2023-04-11 11:06:49 +0900

    수정

--- 03/03.js
+++ 03/03.js
@@ -15,9 +15,21 @@
 
 const show = () => { //show함수에 값 대입 // 화살표 함수로 지정(요즘 많이 사용함)
     event.preventDefault();
-    document.getElementById("h2Id").innerHTML = "안녕하세요." ;
+    // document.getElementById("h2Id").innerHTML = "안녕하세요." ;
+    document.getElementById("h2Id").innerHTML = "<img src = '../img/1.png'></img> "; //이미지 삽입
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("h2Id").innerHTML = "시작입니다." ;
-}); //돔컨텐트가 로드되면 (돔트리가 전체 구성 후) 어떤 이벤트가 발생하는건가?라고 지정해줌.
+// document.addEventListener("DOMContentLoaded", function(){
+//     document.getElementById("h2Id").innerHTML = "시작입니다." ;
+// }); //돔컨텐트가 로드되면 (돔트리가 전체 구성 후) 어떤 이벤트가 발생하는건가?라고 지정해줌.
+
+document.addEventListener("DOMContentLoaded", () => { //위의 코드를 화살표 함수로 지정하여 표현한 방식
+    // document.getElementById("h2Id").innerHTML = "시작입니다." ;
+
+    // document.querySelector('#h2Id').innerHTML = <"아이디 선택자 : 시작입니다."> ; // = document.getElementById("h2Id").innerHTML
+    // document.querySelector('.h2class').innerHTML = "클래스 선택자 : 시작입니다.";
+    // const bt = document.createElement('button'); //a라는 버튼을 생성.
+    // bt.textContent = "확인" ; //버튼의 이름 지정.
+    // document.getElementById('hdiv').append(bt); //hdiv위치에 버튼을 추가(append)하는것.
+
+});
