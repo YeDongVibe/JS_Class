2bbe8ea73ed69a2fde5fe284c60cb309ad52fd69
YeDongVibe <wkwhstladmss@naver.com>
2023-04-12 10:27:45 +0900

    수정

--- 04/04.js
+++ 04/04.js
@@ -1,7 +1,20 @@
-const show = () => { 
+const show = () => {
     event.preventDefault();
 
     //랜덤수 생성 (1~6)
-    let n = Math.floor(Math.random() * 6) + 1 ;
-    console.log(n); 
+    let n = Math.floor(Math.random() * 6) + 1;
+    document.querySelector(".h2class").innerHTML = `<img src = "../img/${n}.png">`;
+
+    //사용자 입력 수 : 라디오의 체크값을 찾음
+    let a = document.querySelector('input[name="num"]:checked').value;
+
+    //검사&결과 출력
+    if (a == n) {
+        document.querySelector('#h3Id').innerText = '정답입니다.';
+    }
+    else {
+        document.querySelector('#h3Id').innerText = '땡.';
+    }
+
+    console.log(n);
 }
