d71500202becb6eb4355716d2c4dc02a64e818cb
YeDongVibe <wkwhstladmss@naver.com>
2023-04-14 13:10:58 +0900

    폭탄

---
+++ 05/05_2.js
@@ -0,0 +1,12 @@
+<img src="../img/boom.png" />
+
+//reset기능 구현
+btReset.addEventListener('click', (event) => {
+    event.preventDefault();
+    t1.value = '';
+    arr = [];
+})
+
+let num = [0, 0, 0, 0, 0, 0, 0, 0, 1];
+let boom = false;
+let heart = 0;
